dd6f05b2a8e6f38ab46386fa92455324e5ecb138
Thomas D Williams <dwilliams@halehan.com>
2017-09-18 09:19:41 -0500

    Changed code

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,9 +5,8 @@ from botocore.client import Config
 import mimetypes
 
 def lambda_handler(event, context):
-
-     print "Started Lambda Function"
   
+
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:772417646419:deployPortfolioTopic')
     
@@ -41,16 +40,16 @@ def lambda_handler(event, context):
                 portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
                 
-                print "job complete!"
+                print str(nm) + " Pushed to build location"
     
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio was deployed Successfully")
         
         if job:
             codepipeline = boto3.client('codepipeline')
-            codepipeline = put_job_success_result(jobId=job["id"])
+            codepipeline.put_job_success_result(jobId=job["id"])
             
     except:
         topic.publish(Subject="Portfolio Deployment Error", Message="Portfolio was NOT deployed Successfully")
         raise
   
-    return 'Hello from Lambda'
+    return 'Deployment Complete'
