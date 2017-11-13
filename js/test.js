c35634c7b905ad71f29747dd1063692f3cbf6ee4
Thomas D Williams <dwilliams@halehan.com>
2017-11-13 15:54:30 -0600

    Added another React Component

---
+++ js/test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Test extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+    render(){
+        return (
+           <h2>Cumbersome</h2>
+        )
+    }
+}
+
+
+export default Test;
+
